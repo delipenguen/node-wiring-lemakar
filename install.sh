2bb62d22576cd0782bc78d1e076aacd720f5ff3a
delipenguen <csemih@gmail.com>
2024-02-23 21:25:12 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 rm ./install.log 2>/dev/null 1>&2
 
-echo "Building node-wiring-pi ... "
+echo "Building node-wiring-lemaker ... "
 node-gyp clean 2>&1 | tee -a ./install.log
 node-gyp configure build 2>&1 | tee -a ./install.log
